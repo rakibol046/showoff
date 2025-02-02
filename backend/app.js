5f6000e2b46019d7d7a06eb9d0fe580885b07462
rakibol046 <mdrakibolislam046@gmail.com>
2025-02-02 14:26:06 +0600

    change module

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, async () => {
+  await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/showoff-db');
+  console.log(`---App is Running---`);
+});
